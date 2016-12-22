cf6e9007c669fcb6db42aec747fae18f71553704
Max Gautier <mgautier@student.42.fr>
2016-12-22 19:12:05 +0100

    Update main test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 16:29:38 by mgautier          #+#    #+#             */
-/*   Updated: 2016/12/22 15:12:55 by chbechet         ###   ########.fr       */
+/*   Updated: 2016/12/22 18:37:31 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,13 @@ void	ft_display_char_lst(t_lst *elem)
 {
 	ft_putendl((char*)elem->content);
 }
+t_bool	ft_valid_piece(const t_lst *link)
+{
+	char *piece;
+
+	piece = (char*)link->content;
+	return (ft_valid_count(piece) && ft_good_place_back_n(piece));
+}
 
 int	main(int argc, char **argv)
 {
@@ -32,8 +39,12 @@ int	main(int argc, char **argv)
 	if (fd <= 0)
 		return (1);
 	piece_lst = ft_file_reader(fd);
+	if (piece_lst == NULL)
+		return (1);
 	printf("%p begin \n %p end \n", piece_lst->begin_lst, piece_lst->end_lst);
 	f_lstiter(piece_lst->begin_lst, &ft_display_char_lst);
+	if (f_lst_every_valid(piece_lst->begin_lst, &ft_valid_piece) != NULL)
+		return (1);
 	close(fd);
 	return (0);
 }
