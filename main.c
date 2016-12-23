cf165f2280a6a04e0181e6402cb828dcef060e9d
Max Gautier <mgautier@student.42.fr>
2016-12-23 19:01:49 +0100

    Remove ft_transform, has been put inside the main project

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 16:29:38 by mgautier          #+#    #+#             */
-/*   Updated: 2016/12/23 15:01:26 by mgautier         ###   ########.fr       */
+/*   Updated: 2016/12/23 18:27:34 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 #include "libft.h"
 #include "fillit.h"
 
-t_rel_position	*ft_string_to_coordinates(char *str);
 void	ft_display_char_lst(t_lst *elem)
 {
 	ft_putendl((char*)elem->content);
@@ -43,16 +42,6 @@ void	ft_display_piece_coord(t_rel_position *piece)
 		ft_putchar('\n');
 		index++;
 	}
-}
-
-t_lst	*ft_transform(t_lst *piece)
-{
-	t_lst *new_link;
-
-	new_link = f_lstnew(NULL);
-	if (new_link != NULL)
-		new_link->content = ft_string_to_coordinates((char*)piece->content);
-	return (new_link);
 }
 
 void	ft_display(t_lst *piece)
@@ -81,7 +70,7 @@ int	main(int argc, char **argv)
 	if (f_lst_every_valid(piece_lst->begin_lst, &ft_valid_pieces_format) != NULL)
 		return (1);
 	close(fd);
-	pieces = f_lstmap(piece_lst->begin_lst, &ft_transform);
+	pieces = f_lstmap(piece_lst->begin_lst, &ft_transform_into_coord);
 	f_lstiter(pieces, &ft_display);
 	return (0);
 }
