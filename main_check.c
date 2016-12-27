b5f7f535bad2f21d83616b3b2e4c09ddd60c1833
Max Gautier <mgautier@student.42.fr>
2016-12-27 15:04:52 +0100

    Update test for ft_check_count

--- main_check.c
+++ main_check.c
@@ -6,28 +6,28 @@
 /*   By: chbechet <chbechet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/26 12:11:53 by chbechet          #+#    #+#             */
-/*   Updated: 2016/12/26 12:17:47 by chbechet         ###   ########.fr       */
+/*   Updated: 2016/12/27 15:01:31 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_reader.h"
 #include "libft.h"
 #include <fcntl.h>
 #include <stdio.h>
 #define BUF_SIZE 20
 
+unsigned int	ft_check_count(const char *piece);
 int		main(int argc, char **argv)
 {
 	int fd;
 	int i;
-	char buf[BUF_SIZE + 1];
+	char	*piece;
 
 	i = 0;
+	if (argc == 1)
+		return (1);
 	fd = open(argv[1], O_RDONLY);
-	while ((i = read(fd, buf, BUF_SIZE)))
-	{
-		buf[i] = '\0';
-		printf("%s", buf);
-	}
-	ft_check_piece(buf);
+	piece = ft_piece_reader(fd);
+	printf("%s\n%u\n", piece, ft_check_count(piece));
 	return (0);
 }
