5d38317cb7e85aa08b6281ea3ca82e88af6b11c2
Max Gautier <mgautier@student.42.fr>
2017-01-04 18:00:35 +0100

    Test multi-fd

--- main.c
+++ main.c
@@ -6,27 +6,50 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 16:29:38 by mgautier          #+#    #+#             */
-/*   Updated: 2017/01/04 12:54:49 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/01/04 17:42:55 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <fcntl.h>
+#include <stdio.h>
+
 int	get_next_line(const int fd, char **line);
 
 int	main(int argc, char **argv)
 {
-	int	file_to_read;
+	int		*file_to_read;
+	int		arg_count;
 	char	*line;
+	t_bool	files_left;
+	t_bool	file_not_over;
 
+	file_to_read = (int*)malloc(sizeof(int) * (argc - 1));
 	if (argc == 1)
 		file_to_read = STDIN_FILENO;
 	else
-		file_to_read = open(argv[1], O_RDONLY);
-	while (get_next_line(file_to_read, &line) == 1)
 	{
-		ft_putendl(line);
-		ft_strdel(&line);
+		arg_count = 0;
+		while (arg_count + 1 < argc)
+		{
+			file_to_read[arg_count] = open(argv[arg_count + 1], O_RDONLY);
+			arg_count++;
+		}
+	}
+	files_left = TRUE;
+	while (files_left)
+	{
+		files_left = FALSE;
+		arg_count = 0;
+		while (arg_count + 1 < argc)
+		{
+			file_not_over = get_next_line(file_to_read[arg_count], &line) == 1;
+			files_left = file_not_over || files_left;
+			if (file_not_over)
+				ft_putendl(line);
+			ft_strdel(&line);
+			arg_count++;
+		}
 	}
 	return (0);
 }
