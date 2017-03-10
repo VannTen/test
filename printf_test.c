de2c40503f8012e57f980158949193af14fea5f9
Max Gautier <mgautier@student.42.fr>
2017-03-10 18:30:17 +0100

    tests' '

--- printf_test.c
+++ printf_test.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/31 17:19:20 by mgautier          #+#    #+#             */
-/*   Updated: 2017/03/07 13:51:17 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/03/10 17:52:39 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 #include <limits.h>
 #include <stdint.h>
 #include <locale.h>
+#include <stdlib.h>
 
 int main(void)
 {
+	int index;
 	setlocale(LC_ALL, "");
-//	printf("|%1$*2$d| |%1$*1$d| |%1$*3$d|", 3, 8,960);
-//	printf("%wd %w");
-	printf("q%5d%k\n", 6);
-
+	//	printf("|%1$*2$d| |%1$*1$d| |%1$*3$d|", 3, 8,960);
+	//	printf("%wd %w");
+	if (malloc(0) == NULL)
+	{
+		index = printf("%d", 56);
+		printf("return is %d\n", index);
+	}
 	return (0);
 }
 
