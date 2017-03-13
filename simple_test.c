ad6b4e39b7fc00f774f5da96bf7c73a7b805a544
Max Gautier <mgautier@student.42.fr>
2017-03-13 19:56:03 +0100

    Test for field_width

--- simple_test.c
+++ simple_test.c
@@ -6,15 +6,21 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/10 10:57:43 by mgautier          #+#    #+#             */
-/*   Updated: 2017/03/10 13:49:30 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/03/13 19:34:06 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-#define FMT "%d\n"
-#define ARG_LIST 78
+#include <stdio.h>
+#include <limits.h>
+#define FMT "TRE : %1$lld TRE : %2$*3$djoooooooo\n"
+#define ARG_LIST LLONG_MAX, 3290, -9
 int	main(void)
 {
-	ft_printf(FMT, ARG_LIST);
+	int test;
+	int control;
+	test = ft_printf(FMT, ARG_LIST);
+	control = printf(FMT, ARG_LIST);
+	printf("\nReturn = %d\nControl = %d\n", test, control);
 	return (0);
 }
