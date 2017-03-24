d34aa29d35150e2690c99d4413d33a8db089851e
Max Gautier <mgautier@student.42.fr>
2017-03-24 15:36:47 +0100

    Better tests

--- test.sh
+++ test.sh
@@ -2,73 +2,101 @@
 
 CC=gcc
 fmt_array=(\
-	%d\
-	%o\
-	%i\
-	%u\
-	%x\
-	%X\
-	%D\
-	%U\
-	%O\
-	%p\
-	%c\
-	%s\
-	%lc\
-	%ls\
-	%\#-d\
-	%\#-o\
-	%\#-i\
-	%\#-u\
-	%\#-x\
-	%\#-X\
-	%\#-D\
-	%\#-U\
-	%\#-O\
-	%\#-p\
-	%\#-c\
-	%\#-s\
-	%\#-lc\
-	%\#-ls\
-	dsada%ddesdes\
-	dsada%odesdes\
-	dsada%idesdes\
-	dsada%udesdes\
-	dsada%xdesdes\
-	dsada%Xdesdes\
-	dsada%Ddesdes\
-	dsada%Udesdes\
-	dsada%Odesdes\
-	dsada%pdesdes\
-	dsada%cdesdes\
-	dsada%sdesdes\
-	dsada%lcdesdes\
-	dsada%lsdesdes\
-	dsada%-74.32ddesdes\
-	dsada%-74.32odesdes\
-	dsada%-74.32idesdes\
-	dsada%-74.32udesdes\
-	dsada%-74.32xdesdes\
-	dsada%-74.32Xdesdes\
-	dsada%-74.32Ddesdes\
-	dsada%-74.32Udesdes\
-	dsada%-74.32Odesdes\
-	dsada%-74.32pdesdes\
-	dsada%-74.32cdesdes\
-	dsada%-74.32sdesdes\
-	dsada%-74.32lcdesdes\
-	dsada%-74.32lsdesdes\
-	)
+	'%5d'
+'%5o'
+'%5i'
+'%5u'
+'%5x'
+'%5X'
+'%5D'
+'%5U'
+'%5O'
+'%5p'
+'%5c'
+'%5s'
+'%5lc'
+'%5ls'
+'%#-d'
+'%#o'
+'%#-i'
+'%#-u'
+'%#-x'
+'%#-X'
+'%#-D'
+'%#-U'
+'%#-O'
+'%#-p'
+'%#-c'
+'%#-s'
+'%#-lc'
+'%#-ls'
+'dsada%ddesdes'
+'dsada%odesdes'
+'dsada%idesdes'
+'dsada%udesdes'
+'dsada%xdesdes'
+'dsada%Xdesdes'
+'dsada%Ddesdes'
+'dsada%Udesdes'
+'dsada%Odesdes'
+'dsada%pdesdes'
+'dsada%cdesdes'
+'dsada%sdesdes'
+'dsada%lcdesdes'
+'dsada%lsdesdes'
+'dsada%#0+-74.32ddesdes'
+'dsada%#0+-74.32odesdes'
+'dsada%#0+-74.32idesdes'
+'dsada%#0+-74.32udesdes'
+'dsada%#0+-74.32xdesdes'
+'dsada%#0+-74.32Xdesdes'
+'dsada%#0+-74.32Ddesdes'
+'dsada%#0+-74.32Udesdes'
+'dsada%#0+-74.32Odesdes'
+'dsada%#0+-74.32pdesdes'
+'dsada%#0+-74.32cdesdes'
+'dsada%#0+-74.32sdesdes'
+'dsada%#0+-74.32lcdesdes'
+'dsada%#0+-74.32lsdesdes'
+'dsada%#0+-0.0ddesdes'
+'dsada%#0+-0.0odesdes'
+'dsada%#0+-0.0idesdes'
+'dsada%#0+-0.0udesdes'
+'dsada%#0+-0.0xdesdes'
+'dsada%#0+-0.0Xdesdes'
+'dsada%#0+-0.0Ddesdes'
+'dsada%#0+-0.0Udesdes'
+'dsada%#0+-0.0Odesdes'
+'dsada%#0+-0.0pdesdes'
+'dsada%#0+-0.0cdesdes'
+'dsada%#0+-0.0sdesdes'
+'dsada%#0+-0.0lcdesdes'
+'dsada%#0+-0.0lsdesdes'
+'dsada%#0+20.0ddesdes'
+'dsada%#0+20.0odesdes'
+'dsada%#0+20.0idesdes'
+'dsada%#0+20.0udesdes'
+'dsada%#0+20.0xdesdes'
+'dsada%#0+20.0Xdesdes'
+'dsada%#0+20.0Ddesdes'
+'dsada%#0+20.0Udesdes'
+'dsada%#0+20.0Odesdes'
+'dsada%#0+20.0pdesdes'
+'dsada%#0+20.0cdesdes'
+'dsada%#0+20.0sdesdes'
+'dsada%#0+20.0lcdesdes'
+'dsada%#0+20.0lsdesdes'
+)
 arg_array=(\
-	15\
-	15\
-	15\
-	15\
-	15\
-	15\
-	15\
-	15\
-	15\
+	15,45\
+	15,45\
+	15,45\
+	15,45\
+	15,45\
+	15,45\
+	15,45\
+	15,45\
+	15,45\
 	\(void*\)255\
 	\'k\'\
 	\"string\"\
@@ -76,7 +104,11 @@ arg_array=(\
 	L\"Русский\"\
 	)
 includelib='-iquote.. -iquote../includes'
-debug_flags='-fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer'
+if [ $1 = debug ]
+then
+	debug_make=debug
+	debug_flags='-fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer'
+fi
 error_flags='-g -Wall -Werror -Wextra -Wno-format'
 test_src_file=simple_test.c
 search_lib=-L..
@@ -84,11 +116,11 @@ link_lib=-lft
 count=${#fmt_array[@]}-1
 count_2=${#arg_array[@]}
 
-make -C .. -j -l 2.8 || exit 255
+make $debug_make -C .. -j -l 2.8 || exit 255
 for ((i=0;i<=count;i++))
 do
 	echo -e "\nTesting format_string"  \"${fmt_array[i]}\" with args \(${arg_array[$((i%count_2))]}\)
-	if ! $CC $error_flags\
+	if ! $CC $error_flags $debug_flags\
 		-D FMT=\"${fmt_array[i]}\" -D ARG_LIST=${arg_array[$((i%count_2))]} $includelib $search_lib $link_lib\
 		${test_src_file}
 then
@@ -102,21 +134,26 @@ then
 else
 	echo Succed on format string \"${fmt_array[i]}\" with args \(${arg_array[$((i%count_2))]}\)
 fi
-./a.out leaks > /dev/null &
-sleep 1
-pid=$!
-leaks $pid > /dev/null
-leaks=$?
-kill $pid
-if [ $leaks = 0 ]
+if [ $1 = "debug" ]
 then
-	echo No leaks
-elif [ $leaks = 1 ]
-then
-	echo format string \"${fmt_array[i]}\" with args \(${arg_array[$((i%count_2))]}\) causes leaks !
-	exit 1
+	echo Debugging
 else
-	echo Error with leaks command
-	exit 255
+	./a.out leaks > /dev/null &
+	sleep 1
+	pid=$!
+	leaks $pid > /dev/null
+	leaks=$?
+	kill $pid
+	if [ $leaks = 0 ]
+	then
+		echo No leaks
+	elif [ $leaks = 1 ]
+	then
+		echo format string \"${fmt_array[i]}\" with args \(${arg_array[$((i%count_2))]}\) causes leaks !
+		exit 1
+	else
+		echo Error with leaks command
+		exit 255
+	fi
 fi
 done
