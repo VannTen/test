a3c24b2f8808470b2f9e84dd40c7cd5802ba38de
Max Gautier <mgautier@student.42.fr>
2017-03-24 14:17:22 +0100

    Add leaks verification and some things

--- simple_test.c
+++ simple_test.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/10 10:57:43 by mgautier          #+#    #+#             */
-/*   Updated: 2017/03/23 14:35:16 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/03/24 14:04:22 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,21 @@
 # define FMT "\neeee%drer%derere"
 #endif
 #ifndef ARG_LIST
-# define ARG_LIST 45, 42, L'Ÿ', 45
+# define ARG_LIST 45, 42
 #endif
 
 int	main(int argc, char **argv)
 {
 	int test;
 
-	//setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "");
 	if (argc == 2 && ft_strcmp(argv[1], "true") == 0)
 			test = printf(FMT, ARG_LIST);
+	else if (argc == 2 && ft_strcmp(argv[1], "leaks") == 0)
+	{
+		test = ft_printf(FMT, ARG_LIST);
+		while (1);
+	}
 	else
 		test = ft_printf(FMT, ARG_LIST);
 	printf("\nReturn value : %d\n", test);
--- test.sh
+++ test.sh
@@ -16,6 +16,20 @@ fmt_array=(\
 	%s\
 	%lc\
 	%ls\
+	%\#-d\
+	%\#-o\
+	%\#-i\
+	%\#-u\
+	%\#-x\
+	%\#-X\
+	%\#-D\
+	%\#-U\
+	%\#-O\
+	%\#-p\
+	%\#-c\
+	%\#-s\
+	%\#-lc\
+	%\#-ls\
 	dsada%ddesdes\
 	dsada%odesdes\
 	dsada%idesdes\
@@ -30,6 +44,20 @@ fmt_array=(\
 	dsada%sdesdes\
 	dsada%lcdesdes\
 	dsada%lsdesdes\
+	dsada%-74.32ddesdes\
+	dsada%-74.32odesdes\
+	dsada%-74.32idesdes\
+	dsada%-74.32udesdes\
+	dsada%-74.32xdesdes\
+	dsada%-74.32Xdesdes\
+	dsada%-74.32Ddesdes\
+	dsada%-74.32Udesdes\
+	dsada%-74.32Odesdes\
+	dsada%-74.32pdesdes\
+	dsada%-74.32cdesdes\
+	dsada%-74.32sdesdes\
+	dsada%-74.32lcdesdes\
+	dsada%-74.32lsdesdes\
 	)
 arg_array=(\
 	15\
@@ -48,16 +76,19 @@ arg_array=(\
 	L\"Русский\"\
 	)
 includelib='-iquote.. -iquote../includes'
+debug_flags='-fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer'
+error_flags='-g -Wall -Werror -Wextra -Wno-format'
 test_src_file=simple_test.c
 search_lib=-L..
 link_lib=-lft
 count=${#fmt_array[@]}-1
 count_2=${#arg_array[@]}
 
-make -C .. debug -j -l 2.8
+make -C .. -j -l 2.8 || exit 255
 for ((i=0;i<=count;i++))
 do
-	if ! $CC -g -Wall -Werror -Wextra -fsanitize=address -fsanitize=undefined\
+	echo -e "\nTesting format_string"  \"${fmt_array[i]}\" with args \(${arg_array[$((i%count_2))]}\)
+	if ! $CC $error_flags\
 		-D FMT=\"${fmt_array[i]}\" -D ARG_LIST=${arg_array[$((i%count_2))]} $includelib $search_lib $link_lib\
 		${test_src_file}
 then
@@ -70,5 +101,22 @@ then
 	exit 1
 else
 	echo Succed on format string \"${fmt_array[i]}\" with args \(${arg_array[$((i%count_2))]}\)
+fi
+./a.out leaks > /dev/null &
+sleep 1
+pid=$!
+leaks $pid > /dev/null
+leaks=$?
+kill $pid
+if [ $leaks = 0 ]
+then
+	echo No leaks
+elif [ $leaks = 1 ]
+then
+	echo format string \"${fmt_array[i]}\" with args \(${arg_array[$((i%count_2))]}\) causes leaks !
+	exit 1
+else
+	echo Error with leaks command
+	exit 255
 fi
 done
